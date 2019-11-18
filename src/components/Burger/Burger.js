6efd23424a08689b1f1350a7190ef1d2d138748f
Ilya Aldabaev <aldabaev2001@gmail.com>
2019-11-18 11:45:09 +0100

    commit_dynamically_ingr2

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,14 +4,19 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey}/>
             })
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        },[]);
 
-    //console.log([...Array(props.ingredients['cheese'])]);
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Pleas start adding ingredients!</p>
+    }
 
     return(
         <div className={classes.Bureger}>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,9 +7,9 @@ class BurgerBuilder extends Component{
     state = {
         ingredients: {
             salad: 1,
-            bacon: 1,
+            bacon: 2,
             cheese: 2,
-            meat: 2
+            meat: 1
         }
     };
 
